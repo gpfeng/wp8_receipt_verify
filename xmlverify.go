d9b53e951a1a3422b0c80c9e2c719925f51ea796
fengguangpu <fengguangpu@nibirutech.com>
2015-03-10 22:39:01 +0800

    remove useless code

--- xmlverify.go
+++ xmlverify.go
@@ -78,52 +78,6 @@ func (att canonAtt) Less(i, j int) bool {
 	return att[i].Name.Local < att[j].Name.Local
 }
 
-// An XML Signature.
-type Signature struct {
-	XMLName        xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# Signature"`
-	SignedInfo     SignedInfo
-	SignatureValue string `xml:"http://www.w3.org/2000/09/xmldsig# SignatureValue"`
-	KeyInfo        KeyInfo
-}
-
-// An algorithm used when creating the signature.
-type Algorithm struct {
-	Algorithm string `xml:",attr"`
-}
-
-// Information about the Signature used for verification.
-type SignedInfo struct {
-	XMLName                xml.Name  `xml:"http://www.w3.org/2000/09/xmldsig# SignedInfo"`
-	CanonicalizationMethod Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# CanonicalizationMethod"`
-	SignatureMethod        Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# SignatureMethod"`
-	Reference              Reference
-}
-
-// Reference to the signed node.
-type Reference struct {
-	XMLName      xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# Reference"`
-	URI          string   `xml:",attr,omitempty"`
-	Transforms   Transforms
-	DigestMethod Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# DigestMethod"`
-	DigestValue  string    `xml:"http://www.w3.org/2000/09/xmldsig# DigestValue"`
-}
-
-// Transforms applied that should be applied to the document prior to signature verification.
-type Transforms struct {
-	XMLName   xml.Name    `xml:"http://www.w3.org/2000/09/xmldsig# Transforms"`
-	Transform []Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# Transform"`
-}
-
-type KeyInfo struct {
-	XMLName  xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# KeyInfo"`
-	X509Data X509Data
-}
-
-type X509Data struct {
-	XMLName         xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# X509Data"`
-	X509Certificate string   `xml:"http://www.w3.org/2000/09/xmldsig# X509Certificate"`
-}
-
 func digestBase64(data string) string {
     return base64.StdEncoding.EncodeToString(digest(data))
 }
